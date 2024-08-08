2f4025c0f6e30a3b3e871e2e5095ed8d138cb729
dxuglas <76638123+dxuglas@users.noreply.github.com>
2024-08-08 13:26:44 +1200

    setup control class and drive and intake controls

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,7 +1,52 @@
 #include "main.h"
 
+class Controls {
+	#define INTAKE_FORWARD pros::E_CONTROLLER_DIGITAL_L1
+	#define INTAKE_REVERSE pros::E_CONTROLLER_DIGITAL_L2
+	#define LIFT_UP pros::E_CONTROLLER_DIGITAL_R1
+	#define LIFT_DOWN pros::E_CONTROLLER_DIGITAL_R2
+
+	public:
+		void update() {
+			update_drive();
+			update_intake();
+		}
+	private:
+		int drive_direction = 1; 
+		int left_joystick_dead_zone = 1;
+		int right_joystick_dead_zone = 1;
+
+		void update_drive() {
+			if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) {
+				// Set drive direction to inverse if button toggle is pressed
+				drive_direction *= -1;
+			}
+
+			float left_y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+			float right_y = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+
+			if (left_y < left_joystick_dead_zone) left_y = 0; // Left dead zone
+			if (right_y < right_joystick_dead_zone) right_y = 0; // Right dead zone
+			
+			drive_left.move(left_y * drive_direction * 127); // Left drive velocity
+			drive_right.move(right_y * drive_direction * 127); // Right drive velocity
+		}
+
+		void update_intake() {
+			if (master.get_digital(INTAKE_FORWARD)) {
+				intake.move(127);
+			} else if (master.get_digital(INTAKE_REVERSE)) {
+				intake.move(-127);
+			} else {
+				intake.move(0);
+			}
+		}
+};
+
 void opcontrol() {
-	while (true) {
+	Controls controls;
+ 	while (true) {
+		controls.update();
 		pros::delay(10);
 	}
 }
